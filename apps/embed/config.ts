eb18336af1dc92a263fe99e100c286e7ef7a6cd6
Ashish Jha <ashishxyzjha@gmail.com>
2026-07-20 02:17:11 +0530

    Added embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_3GS7cagwj2fNKcKMUS9R7ZkyRms",
+  DEFAULT_POSITION: "bottom-right" as const,
+};
