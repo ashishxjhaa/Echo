79a5397cd05669b5a8c7694d4963cceccc015161
Ashish Jha <ashishxyzjha@gmail.com>
2026-07-19 18:47:52 +0530

    Added widget config

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -118,12 +118,12 @@ export const create = mutation({
     //   contactSessionId: args.contactSessionId,
     // })
 
-    // const widgetSettings = await ctx.db
-    //   .query("widgetSettings")
-    //   .withIndex("by_organization_id", (q) =>
-    //     q.eq("organizationId", args.organizationId)
-    //   )
-    //   .unique()
+    const widgetSettings = await ctx.db
+      .query("widgetSettings")
+      .withIndex("by_organization_id", (q) =>
+        q.eq("organizationId", args.organizationId)
+      )
+      .unique()
 
     const { threadId } = await supportAgent.createThread(ctx, {
       userId: args.organizationId,
@@ -133,7 +133,7 @@ export const create = mutation({
       threadId,
       message: {
         role: "assistant",
-        content: "Hello, how can I help you today?",
+        content: widgetSettings?.greetMessage || "Hello, how can I help you today?",
       },
     })
 
