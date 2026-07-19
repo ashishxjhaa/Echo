6a90907b1526eb1ee8ca544f4318a91660dda966
Ashish Jha <ashishxyzjha@gmail.com>
2026-07-20 00:45:03 +0530

    API Improvement

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,5 +1,5 @@
 import { mutation, query } from "../_generated/server"
-import { components } from "../_generated/api"
+import { components, internal } from "../_generated/api"
 import { ConvexError, v } from "convex/values"
 import { supportAgent } from "../system/ai/agents/supportAgent"
 import { MessageDoc, saveMessage } from "@convex-dev/agent"
@@ -114,9 +114,9 @@ export const create = mutation({
     }
 
     // This refreshes the user's session if they are within the threshold
-    // await ctx.runMutation(internal.system.contactSessions.refresh, {
-    //   contactSessionId: args.contactSessionId,
-    // })
+    await ctx.runMutation(internal.system.contactSessions.refresh, {
+      contactSessionId: args.contactSessionId,
+    })
 
     const widgetSettings = await ctx.db
       .query("widgetSettings")
